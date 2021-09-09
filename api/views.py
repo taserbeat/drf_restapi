e42db5a322ad3287b5b794654d8d4deef7cf965d
taserbeat <taserbeat@gmail.com>
2021-09-09 21:26:06 +0900

    タスクモデルとタグモデルのViewSetを作成

--- api/views.py
+++ api/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from .models import Task, Tag
+from rest_framework import viewsets
+from .serializers import TaskSerializer, TagSerializer
 
-# Create your views here.
+
+class TagViewSet(viewsets.ModelViewSet):
+    """
+    タグモデルのビューセット
+
+    viewsets.ModelViewSetを継承されているので、タグモデルに対するCRUD操作を提供する。
+    """
+
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    """
+    タスクモデルのビューセット
+
+    タスクモデルに対するCRUD操作を提供する。
+    """
+
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
